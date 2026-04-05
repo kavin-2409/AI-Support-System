03f2df77dd6c6e18295c2e2897f93c731b558aee
kavin-2409 <akavin2024@gmail.com>
2026-04-05 18:22:05 +0530

    First Commit

---
+++ support-system/src/test/java/com/project/support_system/SupportSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.support_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = SupportSystemApplication.class)
+class SupportSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
